5c493eec5e8fc4dc216738404b047bd02f23d0da
krishna <33350985+kushwaha03@users.noreply.github.com>
2023-12-18 19:40:53 +0530

    Initial Commit

---
+++ Task/TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskApp.swift
+//  Task
+//
+//  Created by Radhe on 18/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
